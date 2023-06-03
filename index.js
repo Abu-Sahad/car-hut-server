e38e7bb456873bded33e428a353e61c78efbbc39
Abu-Sahad <abusahad603@gmail.com>
2023-06-03 17:38:44 +0600

    all data get on mongo

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
-console.log(process.env.DB_USER)
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivpd0t4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,12 +23,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const serviceCollection = client.db('carDoctor').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        //await client.close();
     }
 }
 run().catch(console.dir);
