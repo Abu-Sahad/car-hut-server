ebf3a1094828c183c3127df11c93726381f8bd32
Abu-Sahad <abusahad603@gmail.com>
2023-06-03 17:47:42 +0600

    single data get on mongo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivpd0t4.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,7 +32,12 @@ async function run() {
             res.send(result)
         })
 
-
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
